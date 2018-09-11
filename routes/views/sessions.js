416fae067fe9a7f71f37ec1b7c611ac5aaba8dfe
Tariq Nazar <tariqfnazar@gmail.com>
2018-09-11 21:04:47 +0200

    first commit

---
+++ routes/views/sessions.js
@@ -0,0 +1,18 @@
+var keystone = require('keystone');
+
+exports = module.exports = function (req, res) {
+
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+
+	// Set locals
+	locals.section = 'sessions';
+	locals.filters = {
+		post: req.params.post,
+	};
+
+	locals.data = {};
+
+	// Render the view
+	view.render('sessions');
+};
