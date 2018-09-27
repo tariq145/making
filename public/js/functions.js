7aaf242d76a5f1466c0e71e3cf1643b7a4415ee6
Tariq Nazar <tariq.nazar13@gmail.com>
2018-09-27 17:32:45 +0200

    git push

--- public/js/functions.js
+++ public/js/functions.js
@@ -6,14 +6,12 @@ document.addEventListener('scroll', function (e) {
    let yPos = body.getBoundingClientRect().y;
 
    if(yPos < -10) {
-      nav.style.backgroundColor = '#0D1323';
-      nav.style.height = '60px';
+      nav.classList.add('scrolling');
       navList.style.padding= '5px 18px';
       logo.style.width = '340px';
 
    }else{
-      nav.style.backgroundColor = '';
-      nav.style.height = '62px';
+      nav.classList.remove('scrolling');
       navList.style.padding= '10px 18px';
       logo.style.width = '350px';
    }
@@ -37,23 +35,17 @@ function scroller(el) {
 let isScrolled = function (el){
    let rect = el.getBoundingClientRect()
    let elTop = rect.top;
+   console.log(elTop - window.innerHeight);
    let isVisible = elTop - window.innerHeight <= 0;
 
    return isVisible;
 }
 
 document.addEventListener('scroll', function(){
-   let collab = document.getElementsByClassName('logo-container')[0];
-
    let feed = document.getElementsByClassName('feed-container')[0];
 
-   let collabIS = false;
    let feedIS = false;
 
-   if(isScrolled(collab) && !collabIS){
-      collab.classList.add('scrolled');
-      collabIS = true;
-   }
    if(isScrolled(feed) && !feedIS){
       feed.classList.add('scrolled');
       feedIS = true;
