273e807e25241682342a9af5a5fb1184cc2f242a
Tariq Nazar <tariq.nazar13@gmail.com>
2018-09-30 19:23:36 +0200

    added image slider on contact

--- routes/views/contact.js
+++ routes/views/contact.js
@@ -1,7 +1,8 @@
 var keystone = require('keystone');
 var Enquiry = keystone.list('Enquiry');
+var Images = keystone.list('Image');
 
-exports = module.exports = function (req, res) {
+module.exports = function (req, res) {
 
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
@@ -12,6 +13,8 @@ exports = module.exports = function (req, res) {
 	locals.formData = req.body || {};
 	locals.validationErrors = {};
 	locals.enquirySubmitted = false;
+
+	view.query('images', Images.model.find());
 
 	// On POST requests, add the Enquiry item to the database
 	view.on('post', { action: 'contact' }, function (next) {
